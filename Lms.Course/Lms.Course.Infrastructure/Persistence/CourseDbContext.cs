fb89805e1e0c07449a11b801c774a787b6ab0867
Setsu <setea.music@gmail.com>
2026-05-18 22:34:02 +0200

    Ssetup course backend structure

---
+++ Lms.Course/Lms.Course.Infrastructure/Persistence/CourseDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Lms.Course.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lms.Course.Infrastructure.Persistence;
+
+public class CourseDbContext : DbContext
+{
+    public CourseDbContext(DbContextOptions<CourseDbContext> options) : base(options)
+    {
+    }
+    public DbSet<CourseEntity> Courses { get; set; } = null!;
+}
